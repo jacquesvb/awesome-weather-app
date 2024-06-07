26b1318a6c340a351f3ae82c38b1946cc7285cad
Jacques Van Blokland <jacquesvb@live.com>
2024-06-06 21:38:19 -0700

    Added services

--- src/services/geoCodeService.js
+++ src/services/geoCodeService.js
@@ -1,3 +1,15 @@
 import axios from "axios";
 
 const GEOCODE_API_URL = "https://geocoding-api.open-meteo.com/v1/search?name=";
+
+const API = axios.create({
+  baseURL: GEOCODE_API_URL,
+});
+
+export const getGeoCode = (name) => {
+  API.get(`${encodeURIComponent(name)}&count=5&language=en&format=json`).then(
+    ({ data }) => data
+  );
+};
+
+// Geocode API: "https://geocoding-api.open-meteo.com/v1/search?name=Salem&count=5&language=en&format=json"
--- src/services/mapService.js
+++ src/services/mapService.js
@@ -1,3 +1,15 @@
 import axios from "axios";
 
 const MAP_API_URL = "https://static-maps.yandex.ru/1.x/?lang=en-US&ll=";
+
+const API = axios.create({
+  baseURL: MAP_API_URL,
+});
+
+export const getMap = (lat, lon, width, height) => {
+  API.get(`${lon},${lat}&z=9&l=map&size=${width},${height}`).then(
+    ({ data }) => data
+  );
+};
+
+// `${mapApi}${lon},${lat}&z=9&l=map&size=600,300`;
